1b48cd3a4d35db5fd559564ee7396e9c013898a7
Cayman <caymannava@gmail.com>
2020-07-14 11:30:27 -0500

    feat: update publish function signature
    BREAKING CHANGE: the publish function signature now takes a single
    message as its second parameter, instead of optionally an array of
    messages.

--- src/index.js
+++ src/index.js
@@ -314,11 +314,11 @@ class PubsubBaseProtocol extends EventEmitter {
    * For example, a Floodsub implementation might simply publish each message to each topic for every peer
    * @abstract
    * @param {Array<string>|string} topics
-   * @param {Array<any>|any} messages
+   * @param {any} message
    * @returns {Promise}
    *
    */
-  publish (topics, messages) {
+  publish (topics, message) {
     throw errcode(new Error('publish must be implemented by the subclass'), 'ERR_NOT_IMPLEMENTED')
   }
 
