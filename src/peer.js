51bd98a35e9ea015f58409f42879ba5a3f0dd1a1
Janko Simonovic <simonovic86@gmail.com>
2020-07-30 15:27:30 +0200

    fix: fix issue introduced with 0.4.7 changes used with ipfs 0.46.0

--- src/peer.js
+++ src/peer.js
@@ -100,11 +100,17 @@ class Peer extends EventEmitter {
     })
     this.conn = conn
 
-    pipe(
+    const piped = pipe(
       this.stream,
       lp.encode(),
       conn
-    ).catch(err => {
+    )
+
+    if (piped == null) {
+      return
+    }
+
+    piped.catch(err => {
       log.error(err)
     })
 
